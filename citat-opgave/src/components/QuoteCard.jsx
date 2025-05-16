50e712d8766e227c011774448305004c8a6f282f
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-05-16 11:04:59 +0200

    QuoteCard created

--- citat-opgave/src/components/QuoteCard.jsx
+++ citat-opgave/src/components/QuoteCard.jsx
@@ -1,3 +1,22 @@
 import styles from "../styles/QuoteCard.module.css";
+import { FaHeart } from "react-icons/fa";
 
-export default function QuoteCard({ quote, addToFavorites }) {}
+export default function QuoteCard({ quote, addToFavorites }) {
+  return (
+    <div className={styles.quote}>
+      <img
+        src="https://picsum.photos/200"
+        alt="{quote.author}"
+        className={styles.randomImage}
+      />
+      <h3>{quote.quote}</h3>
+      <p>
+        <em>{quote.author}</em>
+      </p>
+
+      <button onClick={() => addToFavorites(quote)} className={styles.favBtn}>
+        <FaHeart size={18} />
+      </button>
+    </div>
+  );
+}
